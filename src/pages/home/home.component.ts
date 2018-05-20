81c4367ec98a3a4098e6e31d64364c8563b1c885
dayoni <callmey@nate.com>
2018-05-20 22:39:38 +0900

    Home컴포넌트 따로 만들어서 app.component에서 화면 전환 할 수 있도록 구현.

---
+++ src/pages/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { Router }   from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private router: Router) {}
+  
+    ngOnInit() {
+    }
+
+    /*goLogin() {
+      this.router.navigate(['/login']);
+    }*/
+}
