37f3b2cb1cf4afe1b70a06b53e42071d696b8347
dayoni <callmey@nate.com>
2018-05-21 14:04:12 +0900

    input태그 ngmodel로 수정

--- src/pages/login/login.component.ts
+++ src/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../src/models/user';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+login : User;
+
+users: User[] = [
+  new User (1,1,'callmey',123,'양다연','010-7675-2505','callmey@nate.com',24,1),
+];
+
 
   constructor() { }
 
