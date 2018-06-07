f251357c08b130e30f592603a88a70d62ea07cd9
dayoni <callmey@nate.com>
2018-06-08 00:47:26 +0900

    angular cli, npm 오류로 인한 업뎃 후 재푸시

---
+++ src/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+//import { BrowserModule } from '@angular/platform-browser';
+//import { HttpModule } from '@angular/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() {}
+  
+    ngOnInit() {
+    }
+
+}
---
+++ src/services/user.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Http }       from '@angular/http';
+import { HttpClientModule, HttpClient, HttpHeaders } from '@angular/common/http';
+//import 'rxjs/add/operator/toPromise';
+import { LoginComponent } from '../pages/login/login.component';
+import { User } from '../models/user';
+import {Observable} from 'rxjs';
+
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class UserService {
+    private URL ="http://localhost:8080/art-view/api/";
+    private userUrl = "http://localhost:8080/art-view/"
+
+    constructor(private http: HttpClient) { 
+    }
+
+  SignUp(user: User): Promise<string> {
+    let url = this.URL + 'sign-up';
+    return this.http.post(url, user)
+    .toPromise()
+    //.then(response => response.text() as string)
+    .catch(this.handleError);
+  }
+
+  public login(): Observable<any> {
+    return this.http.get(this.userUrl + 'login');
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+
+  }
+}
