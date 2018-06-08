716b119a7e90007f5cbff4915a98febcfa9445ae
dayoni <callmey@nate.com>
2018-06-09 08:15:35 +0900

    코드 꼬임으로 인한 재푸시

---
+++ src/models/provider.ts
@@ -0,0 +1,17 @@
+export class Provider {
+    id: number;
+    user_id: number;
+    grouping_id: number;
+    fav_district_id: number;
+    major: string;
+    career: string;
+
+    constructor (id, user_id, grouping_id, fav_district_id, major, career){
+            this.id = id;
+            this.user_id = id;
+            this.grouping_id = grouping_id;
+            this.fav_district_id = fav_district_id;
+            this.major = major;
+            this.career = career;
+    }
+}
---
+++ src/pages/create/create1/create.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
