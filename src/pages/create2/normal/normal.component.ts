7f94b7f007619e3eae418f8561a9f1bc13b47c4c
dayoni <callmey@nate.com>
2018-06-11 22:53:20 +0900

    사용자 가입정보 구현

--- src/pages/create2/normal/normal.component.ts
+++ src/pages/create2/normal/normal.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../models/user';
+import { UserService } from '../../../services/user.service';
+import { Artist } from '../../../models/artist';
+import { Provider } from '../../../models/provider';
 
 @Component({
-  selector: 'app-create2',
+  selector: 'app-normal',
   templateUrl: './normal.component.html',
   styleUrls: ['./normal.component.css']
 })
 export class NomalComponent implements OnInit {
-
-  constructor() { }
+  private normal: User;
+  private artist: Artist[]; 
+  private provider: Provider[]; 
+  private userService: UserService[];
+  constructor(normal: User, artist: Artist, provider: Provider, userService: UserService) { }
 
   ngOnInit() {
   }
