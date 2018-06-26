934d43b6c3a470612f0c5f60114035b5109eee0a
dayoni <callmey@nate.com>
2018-06-26 15:15:52 +0900

    회원단계 페이지 수정

--- src/pages/create2/normal/normal.component.ts
+++ src/pages/create2/normal/normal.component.ts
@@ -7,6 +7,7 @@ import { Age } from '../../../models/age';
 import {FormGroup, FormBuilder, Validators, ReactiveFormsModule, FormControl} from '@angular/forms';
 import {MatDialog } from '@angular/material';
 import {MatInputModule} from '@angular/material/input';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-normal',
@@ -16,8 +17,10 @@ import {MatInputModule} from '@angular/material/input';
 export class NomalComponent implements OnInit {
   private normal: User;
   private ages: Age[];
+  Arr = Array; //Array type captured in a variable
+  num:number = 5;
 
-  constructor(private userService: UserService) { 
+  constructor(private userService: UserService, private router: Router) { 
   }
 
   ngOnInit() {
@@ -26,7 +29,8 @@ export class NomalComponent implements OnInit {
   }
 
     create() {
-    this.userService.SignUp(this.normal).then(
-      message=> alert(message));
+    //this.userService.SignUp(this.normal).then(
+      //message=> alert(message));
+      this.router.navigate(['/create3']);
     }
 }
