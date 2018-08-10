0aef6bf86148f024508bf722f0a567ee7307b0e5
irissun9602 <iris9602@naver.com>
2018-08-10 22:32:32 +0900

    feat: Add space.service.ts

--- src/services/space.service.ts
+++ src/services/space.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+import { Space } from '../models/Space';
+@Injectable()
 export class SpaceService {
 
-  constructor() { }
+  private URL = 'http://localhost:8080/art-view/api/';
+  private http: Http;
+
+  constructor(http: Http) {
+    this.http = http;
+  } 
+  
+  getSpaces(keyword: string): Promise<Space[]> { 
+    return this.http.get(this.URL + "spaceDetail/" + keyword)
+    .toPromise()
+    .then(response => response.json() as Space[])
+    .catch(this.handleError); 
+  } 
+    
+    
+private handleError(error: any): Promise<any> { 
+  console.error('An error occurred', error); // for demo purposes only 
+  return Promise.reject(error.message || error); 
+} 
 }
