7f04de40a17d9a33ec28d3a58759af187b30346b
irissun9602 <iris9602@naver.com>
2018-08-10 21:49:57 +0900

    feat: Add model space.ts

---
+++ src/models/space.ts
@@ -0,0 +1,10 @@
+export class Space {
+
+    title: string;
+    link: string;
+    category: string;
+    description: string;
+    telephone: string;
+    address: string;
+    roadAddress: string;
+}
