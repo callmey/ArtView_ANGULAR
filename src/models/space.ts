aa19a516f235e5e8f182771c59b1467a3c3f880d
irissun9602 <iris9602@naver.com>
2018-08-11 22:21:46 +0900

    fix: Add ;

--- src/models/space.ts
+++ src/models/space.ts
@@ -1,11 +1,11 @@
 export class Space {
 
-    id : number
-    name : string
-    city_id : number
-    provider_id : number
-    cost : number
-    unavailable : Date
-    equipment :string
-    address :string
+    id : number;
+    name : string;
+    city_id : number;
+    provider_id : number;
+    cost : number;
+    unavailable : Date;
+    equipment :string;
+    address :string;
 }
