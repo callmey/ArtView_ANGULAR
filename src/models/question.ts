e2a21a8c792dd79be9fd34da9b7c570234639685
irissun9602 <iris9602@naver.com>
2018-08-15 01:57:28 +0900

    feat: 모델 question 추가

---
+++ src/models/question.ts
@@ -0,0 +1,10 @@
+export class Question{
+     id : number;
+     user_id : number;
+     title : string;
+     content : string;
+     date : Date;
+     type : string;
+     hits : number;
+     file_id : number;
+}
